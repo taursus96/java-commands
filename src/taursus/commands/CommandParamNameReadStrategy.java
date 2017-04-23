77b93338b5e9d030838ead11d2ffb9adadd972ce
taursus96 <taursus96@gmail.com>
2017-04-23 13:04:25 +0200

    added command name/paramName/paramValue/skipToNextParam read strategies

---
+++ src/taursus/commands/CommandParamNameReadStrategy.java
@@ -0,0 +1,20 @@
+package taursus.commands;
+
+public class CommandParamNameReadStrategy implements IReadStrategy {
+
+    @Override
+    public boolean endBeforeChar(char c) {
+        return c == ' ';
+    }
+
+    @Override
+    public boolean endAfterChar(char c) {
+        return false;
+    }
+
+    @Override
+    public void before(StringIterator it) {
+        
+    }
+
+}
---
+++ src/taursus/commands/CommandSkipToNextParamReadStrategy.java
@@ -0,0 +1,20 @@
+package taursus.commands;
+
+public class CommandSkipToNextParamReadStrategy implements IReadStrategy {
+
+    @Override
+    public boolean endBeforeChar(char c) {
+        return false;
+    }
+
+    @Override
+    public boolean endAfterChar(char c) {
+        return c == '-';
+    }
+
+    @Override
+    public void before(StringIterator it) {
+        
+    }
+
+}
